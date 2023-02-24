d5ee48df315cc93cfca5e43a52a03ecadcd38b11
urbanpatrol <33529607+urbanpatrol@users.noreply.github.com>
2023-02-24 17:38:37 +0200

    Employee profiles created with the respective criteria and tested in jest with all tests being successful. Profile include Employee, Engineer, Intern and Manager with update sto the respective js files.

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,24 @@
-// TODO: Write code to define and export the Employee class
+// employee constructor
+class Employee {
+    constructor(name, id, email, role = 'Employee') {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = role;
+    }
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getRole() {
+        return this.role;
+    }
+
+}
+
+module.exports = Employee;
